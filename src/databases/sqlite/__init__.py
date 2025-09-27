a1c6a77e01053af9e3cb690f3780e699a8020ac1
wiselychess <wiselychess@gmail.com>
2025-09-27 20:42:27 +0300

    123

--- src/databases/sqlite/__init__.py
+++ src/databases/sqlite/__init__.py
@@ -6,11 +6,12 @@
 - ORM-модели
 - Сессии
 """
-from .core import engine, async_session
+from .core import engine, async_session, Base
 from .connection import get_db_session
 
 __all__ = [
     "engine",
     "async_session",
     "get_db_session",
+    "Base",
 ]
